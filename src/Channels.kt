7bccdc9f55b756a209590333bc67322515b1f81c
David Merchan <davidmerchan10@gmail.com>
2024-12-22 17:31:38 -0500

    Cambio a usar channels que mejora el performance

--- src/Channels.kt
+++ src/Channels.kt
@@ -1,7 +1,9 @@
 import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import java.util.concurrent.ConcurrentLinkedQueue
+import kotlin.system.measureTimeMillis
 
 fun main() {
     val lockAndUnlock = LockAndUnlock()
@@ -10,7 +12,8 @@ fun main() {
 
 class LockAndUnlock {
 
-    private val queue = ConcurrentLinkedQueue<Notes>() // Cola que esta optimizada para trabajar con coroutinas
+    //private val queue = ConcurrentLinkedQueue<Notes>() // Cola java thread tipo safe thread
+    private val channel = Channel<Notes>() // Cola optimizada para trabajar con coroutinas
     private var produced = 0
     private var consumed = 0
     private var isFinished = false
@@ -19,46 +22,59 @@ class LockAndUnlock {
     private val mutex = Mutex()
 
     fun startProducerAndConsumer() {
-        runBlocking {
-            // Producers
-            launch(Dispatchers.Default) {
-                val producers = List(100_000) {
-                    launch {
-                        val note = NotesUtils.getNote()
-                        if (queue.offer(note)) {
+        val duration = measureTimeMillis {
+            runBlocking {
+                // Producers
+                launch(Dispatchers.Default) {
+                    val producers = List(100_000) {
+                        launch {
+                            val note = NotesUtils.getNote()
+                            channel.send(note)
                             mutex.withLock {
                                 produced ++
                             }
+//                            if (queue.offer(note)) {
+//                                mutex.withLock {
+//                                    produced ++
+//                                }
+//                            }
                         }
                     }
+                    producers.joinAll()
+                    channel.close()
+                    isFinished = true
+                    println("Producers Done")
                 }
-                producers.joinAll()
-                isFinished = true
-                println("Producers Done")
-            }
 
-            // Consumers
-            launch(Dispatchers.Default) {
-                val consumers = List(batch.size) {
-                    launch {
-                        while (!isFinished || queue.isNotEmpty()) {
-                            val note = queue.poll()
-                            if (note != null) {
+                // Consumers
+                launch(Dispatchers.Default) {
+                    val consumers = List(batch.size) {
+                        launch {
+                            for (note in channel) {
                                 mutex.withLock {
                                     consumed ++
                                     batch[it] ++
                                 }
                             }
+//                            while (!isFinished || queue.isNotEmpty()) {
+//                                val note = queue.poll()
+//                                if (note != null) {
+//                                    mutex.withLock {
+//                                        consumed ++
+//                                        batch[it] ++
+//                                    }
+//                                }
+//                            }
                         }
                     }
+                    consumers.joinAll()
+                    println("Consumers Done")
                 }
-                consumers.joinAll()
-                println("Consumers Done")
             }
         }
 
         // Verificando la cantidad de notas producidas y consumidas
-        println("Queue size: ${queue.size}")
+        println("Time: $duration") // 559 ms | 442 ms
         println("Produced: $produced")
         println("Consumed: $consumed")
 
